9af7d87b832b0f3050be39b24b5ab6c5d8107c5f
Ankitsingh Bisht <ankitsinghbisht.work@gmail.com>
2025-08-06 21:55:17 +0530

    Added global exception if email already exists

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsExceotion;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -24,6 +25,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestTO patientRequestTO) {
+        if(patientRepository.existsByEmail(patientRequestTO.getEmail())) {
+            throw new EmailAlreadyExistsExceotion("A patient with this email" + "already exists " + patientRequestTO.getEmail());
+        }
         Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestTO));
         return PatientMapper.toDTO(patient);
     }
