4568e324ed42f4e2cac35d3166c78b5930f62b9d
Ankitsingh Bisht <ankitsinghbisht.work@gmail.com>
2025-08-06 21:35:36 +0530

    init commit with patient creation

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,29 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientRequestTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(patient.getId().toString());
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientResponseDTO;
+    }
+
+    public static Patient toModel(PatientRequestTO patientRequestTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestTO.getName());
+        patient.setEmail(patientRequestTO.getEmail());
+        patient.setAddress(patientRequestTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestTO.getRegisteredDate()));
+        return  patient;
+    }
+}
